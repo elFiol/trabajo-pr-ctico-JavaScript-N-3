a17e1c8bb41d00e5f50300fefe3e2e9f39aff533
Fiol Thiago <fiolthiago9@gmail.com>
2023-11-10 11:49:12 -0300

    arreglamos el punto 4 y agregamos punto 5 (no terminado)

--- js/p-4.js
+++ js/p-4.js
@@ -1,11 +1,14 @@
-const esNumeropar = (numero) => numero % 2 === 0;
-const numeroIngresado = parseInt(prompt("Ingrese un número: "));
-if (isNaN(numeroIngresado)) {
-  document.write("ese no es un numero");
-} else if (esNumeropar(numeroIngresado)) {
-  document.write("El número es par");
-} else if (numeroIngresado === 0) {
-  document.write("es un número neutro");
-} else {
-  document.write("El número no es par");
+function TextoPar() {
+  const esNumeropar = (numero) => numero % 2 === 0;
+  const numeroIngresado = parseInt(prompt("Ingrese un número: "));
+  if (isNaN(numeroIngresado)) {
+    return "ese no es un numero";
+  } else if (numeroIngresado === 0) {
+    return "es un número neutro";
+  } else if (esNumeropar(numeroIngresado)) {
+    return "El número es par";
+  } else {
+    return "El número no es par";
+  }
 }
+document.write(TextoPar());
